59fba4e7b67659668c83156acec300de5d2eded1
Akshay Takkar <akshayt@m3india.in>
2023-09-09 17:55:40 +0530

    Added generator for generating migration file

---
+++ lib/generators/deforest/deforest_generator.rb
@@ -0,0 +1,31 @@
+require 'rails/generators/base'
+require 'rails/generators/migration'
+
+class DeforestGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
+
+  source_root File.expand_path('../templates', __FILE__)
+
+  def copy_migration_file
+    migration_template "migration.rb", "db/migrate/create_deforest_logs.rb", migration_version: migration_version
+  end
+
+  def rails5_and_up?
+    Rails::VERSION::MAJOR >= 5
+  end
+
+  def migration_version
+    if rails5_and_up?
+      "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
+    end
+  end
+
+  def self.next_migration_number(dirname)
+    if ActiveRecord::Base.timestamped_migrations
+      current_time = Time.now.utc
+      current_time.strftime("%Y%m%d%H%M%S")
+    else
+      "%.3d" % (current_numeric_version(dirname) + 1)
+    end
+  end
+end
