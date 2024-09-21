62b4f03f7b642c06debc42b191c7f03ec8dcc23c
Akshay Takkar <akshayt@m3india.in>
2024-09-21 20:23:55 +0530

    Fixed migration template for rails 5 and up

--- lib/generators/deforest/deforest_generator.rb
+++ lib/generators/deforest/deforest_generator.rb
@@ -11,27 +11,26 @@ class DeforestGenerator < Rails::Generators::Base
   end
 
   def copy_initializer_file
-    if rails5_and_up? && Rails.autoloaders.zeitwerk_enabled?
+    if DeforestGenerator.rails5_and_up? && Rails.autoloaders.zeitwerk_enabled?
       copy_file "zeitwerk_enabled_initializer.rb", "config/initializers/deforest.rb"
     else
       copy_file "classic_enabled_initializer.rb", "config/initializers/deforest.rb"
     end
   end
 
-  def rails5_and_up?
+  def self.rails5_and_up?
     Rails::VERSION::MAJOR >= 5
   end
 
   def migration_version
-    if rails5_and_up?
+    if DeforestGenerator.rails5_and_up?
       "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
     end
   end
 
   def self.next_migration_number(dirname)
-    if ActiveRecord::Base.timestamped_migrations
-      current_time = Time.now.utc
-      current_time.strftime("%Y%m%d%H%M%S")
+    if (rails5_and_up? && ActiveRecord.timestamped_migrations) || ActiveRecord::Base.timestamped_migrations
+      Time.now.utc.strftime("%Y%m%d%H%M%S")
     else
       "%.3d" % (current_numeric_version(dirname) + 1)
     end
